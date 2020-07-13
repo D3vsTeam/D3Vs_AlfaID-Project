c59198351cc748bf4fb092ac4edfdf4d3f86a792
Arley <arleyaugusto5@gmail.com>
2020-07-13 19:01:36 -0300

    envio da estrutura base da API

---
+++ api/AlphaId/AlphaId.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlphaId.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Identifier { get; set; }
+    }
+}
---
+++ api/AlphaId/AlphaId.Domain/Entities/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlphaId.Domain.Entities
+{
+    public class Manager : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Area { get; set; }
+    }
+}
